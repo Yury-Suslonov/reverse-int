e5c739640ece47e19e2f46897c441101d78f409f
Yury-Suslonov <79038563+Yury-Suslonov@users.noreply.github.com>
2021-03-15 16:03:30 +0300

    feat: update index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
 module.exports = function reverse (n) {
-  
+    f="";
+    n=Math.abs(n);
+    n=String(n);
+    for (let i=n.length-1;i>=0;i--){
+      f+=n[i];
+    }
+    f=Number(f);
+    return f;
 }
